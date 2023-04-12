88b6a92749bcfbf20c09bedc084ea395f77e0072
akinmiday <110313060+akinmiday@users.noreply.github.com>
2023-04-12 19:10:21 +0100

    initial commit

---
+++ tenzies-game/src/components/die.jsx
@@ -0,0 +1,16 @@
+function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "#fff"
+    }
+    return (
+        <div
+            className="die--face"
+            style={styles}
+            onClick={props.holdDice}
+        >
+            <h2 className="die-number" >{props.value}</h2>
+        </div>
+    )
+}
+
+export default Die
